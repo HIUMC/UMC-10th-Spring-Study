f6667279b836caef551252238776da0a94ecf97d
jangmk05 <jangmk05@gmail.com>
2026-03-21 12:18:45 +0900

    [Feat(ellie): 전체 회원 조회 로직 추가]

--- jangmk05/spring-intro/src/main/java/me/moonkyuong/springintro/service/MemberService.java
+++ jangmk05/spring-intro/src/main/java/me/moonkyuong/springintro/service/MemberService.java
@@ -4,6 +4,9 @@ import me.moonkyuong.springintro.domain.Member;
 import me.moonkyuong.springintro.repository.MemberRepository;
 import me.moonkyuong.springintro.repository.MemoryMemberRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public class MemberService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
@@ -21,5 +24,14 @@ public class MemberService {
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
     }
 }
