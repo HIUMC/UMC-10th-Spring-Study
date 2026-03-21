933775f3d327436cb5caa0796b141088aa9ca5ba
jangmk05 <jangmk05@gmail.com>
2026-03-21 10:11:51 +0900

    프로젝트 초기 설정

---
+++ jangmk05/spring-intro/src/test/java/me/moonkyuong/springintro/SpringIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package me.moonkyuong.springintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
