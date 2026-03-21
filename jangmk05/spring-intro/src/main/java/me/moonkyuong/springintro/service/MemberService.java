c0ea6ac41b2bbfeeb6c30278a640addec242e16c
jangmk05 <jangmk05@gmail.com>
2026-03-21 12:16:03 +0900

    [Feat(ellie): 회원가입 로직 추가]

---
+++ jangmk05/spring-intro/src/main/java/me/moonkyuong/springintro/service/MemberService.java
@@ -0,0 +1,25 @@
+package me.moonkyuong.springintro.service;
+
+import me.moonkyuong.springintro.domain.Member;
+import me.moonkyuong.springintro.repository.MemberRepository;
+import me.moonkyuong.springintro.repository.MemoryMemberRepository;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 회원 가입
+    public Long join(Member member) {
+        // 같은 이름이 있는 중복 회원 x
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+}
