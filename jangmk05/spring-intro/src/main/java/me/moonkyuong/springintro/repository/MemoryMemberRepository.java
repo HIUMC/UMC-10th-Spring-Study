bfeb92c52988900c922e968d4aa79f69cc412e7f
jangmk05 <jangmk05@gmail.com>
2026-03-21 13:06:07 +0900

    [Refactor(ellie): 회원 레포지토리 스프링빈 등록]

--- jangmk05/spring-intro/src/main/java/me/moonkyuong/springintro/repository/MemoryMemberRepository.java
+++ jangmk05/spring-intro/src/main/java/me/moonkyuong/springintro/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package me.moonkyuong.springintro.repository;
 
 import me.moonkyuong.springintro.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
