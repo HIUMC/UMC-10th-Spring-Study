bf0d2b0e16f9b1ef77288ea42a66250302a960d9
jangmk05 <jangmk05@gmail.com>
2026-03-22 12:07:31 +0900

    [Chore(ellie): 스프링 데이터 Jpa 회원 레포지토리를 사용하도록 스프링 설정 변경]

--- jangmk05/spring-intro/src/main/java/me/moonkyuong/springintro/service/SpringConfig.java
+++ jangmk05/spring-intro/src/main/java/me/moonkyuong/springintro/service/SpringConfig.java
@@ -1,33 +1,21 @@
 package me.moonkyuong.springintro.service;
 
-import jakarta.persistence.EntityManager;
 import me.moonkyuong.springintro.repository.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
-    private final EntityManager em;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-        // return new MemoryMemberRepository();
-        // return new JdbcMemberRepository(dataSource);
-        // return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+        return new MemberService(memberRepository);
     }
 }
