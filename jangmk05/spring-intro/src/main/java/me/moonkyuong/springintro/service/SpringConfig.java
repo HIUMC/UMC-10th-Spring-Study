a7732e695a978ddcd0ffa398c922641ce3b98d09
jangmk05 <jangmk05@gmail.com>
2026-03-22 11:10:38 +0900

    [Refactor(ellie): 스프링 설정 변경]

--- jangmk05/spring-intro/src/main/java/me/moonkyuong/springintro/service/SpringConfig.java
+++ jangmk05/spring-intro/src/main/java/me/moonkyuong/springintro/service/SpringConfig.java
@@ -1,12 +1,20 @@
 package me.moonkyuong.springintro.service;
 
+import me.moonkyuong.springintro.repository.JdbcMemberRepository;
 import me.moonkyuong.springintro.repository.MemberRepository;
 import me.moonkyuong.springintro.repository.MemoryMemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -15,6 +23,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
