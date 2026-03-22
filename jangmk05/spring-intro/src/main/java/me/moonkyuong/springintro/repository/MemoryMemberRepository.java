ad926a3bf4ce030309963cbb5727435be00ef520
jangmk05 <jangmk05@gmail.com>
2026-03-22 11:09:24 +0900

    [Feat(ellie): Jdbc 회원 레포지토리 구현]

--- jangmk05/spring-intro/src/main/java/me/moonkyuong/springintro/repository/MemoryMemberRepository.java
+++ jangmk05/spring-intro/src/main/java/me/moonkyuong/springintro/repository/MemoryMemberRepository.java
@@ -35,9 +35,5 @@ public class MemoryMemberRepository implements MemberRepository{
         return new ArrayList<>(store.values());
     }
 
-    @Override
-    public void clearStore() {
-        store.clear();
-    }
 
 }
