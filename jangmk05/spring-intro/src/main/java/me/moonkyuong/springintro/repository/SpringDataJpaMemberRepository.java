f85f2093d35b6592884bad8207eb51c4c0d3de74
jangmk05 <jangmk05@gmail.com>
2026-03-22 12:05:43 +0900

    [Feat(ellie): 스프링 데이터 JPA 회원 레포지토리]

---
+++ jangmk05/spring-intro/src/main/java/me/moonkyuong/springintro/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,10 @@
+package me.moonkyuong.springintro.repository;
+
+import me.moonkyuong.springintro.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+    Optional<Member> findByName(String name);
+}
