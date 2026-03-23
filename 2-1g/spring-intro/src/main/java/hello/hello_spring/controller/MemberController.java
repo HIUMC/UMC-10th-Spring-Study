eb1f1c80074b4701a6d4e8744708f8b28dfb5b03
21g <leehan0311@gmail.com>
2026-03-23 16:33:55 +0900

    [Feat(leehan): GetMapping과 PostMapping을 이용하여 회원 등록 및 조회]

--- 2-1g/spring-intro/src/main/java/hello/hello_spring/controller/MemberController.java
+++ 2-1g/spring-intro/src/main/java/hello/hello_spring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hello_spring.controller;
 
+import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 ///  @Controller 어노테이션을 붙이면
 /// 코드를 실행할 때 해당 컨트롤러 객체를 생성하여
@@ -20,9 +26,32 @@ public class MemberController {
     /// @Autowired 어노테이션 사용시 생성자를 이용해 외부에서 객체를 주입받을 때
     /// Spring Container에 있는 객체를 자동으로 연결시켜줌
     /// Autowired를 사용하려면 주입하려는 객체의 코드에도 조치를 취해주어야 함
+
+    private final MemberService memberService;
+
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
-    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String members(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
