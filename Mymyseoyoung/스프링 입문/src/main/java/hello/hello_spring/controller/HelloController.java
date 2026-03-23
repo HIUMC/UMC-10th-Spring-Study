9bdf1aaa87aa3ead28d06403d9ab68c35f3dbcc5
박서영 <emilypark0721@gmail.com>
2026-03-24 02:02:36 +0900

    [Feat](박서영(조이)):섹션 3 완료

--- Mymyseoyoung/스프링 입문/src/main/java/hello/hello_spring/controller/HelloController.java
+++ Mymyseoyoung/스프링 입문/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -21,5 +22,37 @@ public class HelloController {
     {
         model.addAttribute("name",name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    //ResponseBody : 응답 바디부에 이 데이터를 넣어줄 것이다 ! (JSON)
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name)
+    {
+        return "hello "+name;//hello spring ( 이 문자 그대로 내려감 .)
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+
+    public Hello helloApi(@RequestParam("name")String name)
+    {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    //객체 만들기
+    static class Hello
+    {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
