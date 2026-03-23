81ebdb7af397e6d444ad6532d2497f8d6ee1d7aa
j2nooh <wisestorm01@gmail.com>
2026-03-24 07:01:21 +0900

    feat: 자바 코드로 직접 스프링 빈 등록

---
+++ spring-intro/src/main/java/hello/hello_spring/SpringConfig.java
@@ -0,0 +1,22 @@
+package hello.hello_spring;
+
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import hello.hello_spring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    // spring Bean을 등록할 것이라는 의미. @Bean 어노테이션을 사용하여 스프링 컨테이너에 등록한다.
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
