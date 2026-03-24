d3f7f1f36a9c3ddb63fa5b51467c65486d0fdff4
cyunwook <cyunwook@gmail.com>
2026-03-24 16:08:44 +0900

    feat: 스프링 DB 접근 기술 및 JPA 실습 코드 추가

--- cyunwook/스프링 입문/src/main/java/hello/hellospring/controller/MemberController.java
+++ cyunwook/스프링 입문/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,9 +1,9 @@
 package hello.hellospring.controller;
-import ch.qos.logback.core.model.Model;
 import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
--- cyunwook/스프링 입문/src/main/java/hello/hellospring/service/MemberService.java
+++ cyunwook/스프링 입문/src/main/java/hello/hellospring/service/MemberService.java
@@ -2,9 +2,11 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import jakarta.transaction.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository = new
             MemoryMemberRepository();
