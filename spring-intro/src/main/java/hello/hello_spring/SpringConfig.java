8aed3b0c418437f1e15455ef5510275464b75caf
j2nooh <wisestorm01@gmail.com>
2026-03-24 13:24:08 +0900

    feat: Spring JdbcTemplate

--- spring-intro/src/main/java/hello/hello_spring/SpringConfig.java
+++ spring-intro/src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,9 +1,11 @@
 package hello.hello_spring;
 
 import hello.hello_spring.repository.JdbcMemberRepository;
+import hello.hello_spring.repository.JdbcTemplateMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +15,7 @@ public class SpringConfig {
 
     private final DataSource dataSource;
 
+    @Autowired
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
     }
@@ -26,6 +29,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
         // return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+        // return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
