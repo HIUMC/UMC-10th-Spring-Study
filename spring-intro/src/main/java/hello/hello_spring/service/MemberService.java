6ab82ea753babfe28aedc74f5b520626e99e0664
j2nooh <wisestorm01@gmail.com>
2026-03-24 16:17:33 +0900

    feat: AOP 적용

--- spring-intro/src/main/java/hello/hello_spring/service/MemberService.java
+++ spring-intro/src/main/java/hello/hello_spring/service/MemberService.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Optional;
 
 @Transactional
-@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -23,9 +22,17 @@ public class MemberService {
     // 회원가입
     public Long join(Member member) {
 
-        validateDuplicateMember(member); // 중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member); // 중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -37,7 +44,16 @@ public class MemberService {
 
     // 전체 회원 조회
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+
+        long start = System.currentTimeMillis();
+
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId) {
