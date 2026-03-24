98a894a13db25058289bb467e79abfca9ebcfd59
9dongs <144160177+9dongs@users.noreply.github.com>
2026-03-25 03:18:25 +0900

    [Feat(구디) : 순수 JDBC 구현]

--- 9dongs/스프링입문/hello-spring/src/main/java/hello_spring/service/SpringConfig.java
+++ 9dongs/스프링입문/hello-spring/src/main/java/hello_spring/service/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello_spring.service;
 
+import hello_spring.repository.JdbcMemberRepository;
 import hello_spring.repository.MemberRepository;
 import hello_spring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 // 장점 : 나중에 Db 연결시 코드 한줄만 수정하면 싹 연결됨.
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+
+        // return new MemoryMemberRepository(); : 다른 코드 수정 안하고 SpringConfig를 고치는 것 만으로 DI 변경 가능
+        return new JdbcMemberRepository(dataSource);
+        // 객체지향 다향성 활용 -> 개방 폐쇄 원칙 (OCP) ( 기능을 변경해도 기존 코드 변경 x = 확장은 O, 변경,수정은 X )
     }
 }
