58de4fd2ee49807f2e8154742b15880f151a4d62
j2nooh <wisestorm01@gmail.com>
2026-03-24 15:50:52 +0900

    feat: JPA

--- spring-intro/src/main/java/hello/hello_spring/SpringConfig.java
+++ spring-intro/src/main/java/hello/hello_spring/SpringConfig.java
@@ -8,16 +8,20 @@ import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import hello.hello_spring.repository.*;
 
+import jakarta.persistence.EntityManager;
 import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
     private final DataSource dataSource;
+    private final EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
+    public SpringConfig(DataSource dataSource, EntityManager em) {
         this.dataSource = dataSource;
+        this.em = em;
     }
 
     // spring Bean을 등록할 것이라는 의미. @Bean 어노테이션을 사용하여 스프링 컨테이너에 등록한다.
@@ -30,6 +34,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
         // return new MemoryMemberRepository();
         // return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+        // return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
