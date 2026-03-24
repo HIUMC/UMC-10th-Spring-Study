6c656f1409741e4355ed28e95eb90ac3a9f69c45
j2nooh <wisestorm01@gmail.com>
2026-03-24 10:14:12 +0900

    feat: 순수 JDBC

--- spring-intro/src/main/java/hello/hello_spring/SpringConfig.java
+++ spring-intro/src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,13 +1,21 @@
 package hello.hello_spring;
 
+import hello.hello_spring.repository.JdbcMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import hello.hello_spring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     // spring Bean을 등록할 것이라는 의미. @Bean 어노테이션을 사용하여 스프링 컨테이너에 등록한다.
     @Bean
@@ -17,6 +25,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
