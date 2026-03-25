687540bcf7c2f5a41ad33e1bd7596010701f6642
minseo6753 <minseo6753@naver.com>
2026-03-26 07:05:58 +0900

    [Feat(minseo6753/스프링 입문): JPA

--- minseo6753/스프링 입문/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ minseo6753/스프링 입문/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -4,9 +4,9 @@ import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
