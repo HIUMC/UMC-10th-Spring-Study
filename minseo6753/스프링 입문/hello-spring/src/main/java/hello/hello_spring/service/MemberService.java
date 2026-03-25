4b3088e173f5d8167033389666cc7c5f22d04d11
minseo6753 <minseo6753@naver.com>
2026-03-26 07:41:16 +0900

    [Feat(minseo6753/스프링 입문): AOP가 필요한 상황

--- minseo6753/스프링 입문/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ minseo6753/스프링 입문/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -19,10 +19,18 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member) {
-        //같은 이름이 있는 중복 회원X
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
+
+        long start = System.currentTimeMillis();
+
+        try{
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+        } finally { //예외가 터져도 측정
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + " ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
