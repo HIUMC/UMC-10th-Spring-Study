dadb39613e451668753a2635c7dcd4fc0c5aa31c
minseo6753 <minseo6753@naver.com>
2026-03-26 05:59:18 +0900

    [Feat(minseo6753/스프링 입문): 회원 웹 기능 - 조회

--- minseo6753/스프링 입문/hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
+++ minseo6753/스프링 입문/hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
@@ -2,10 +2,12 @@ package hello.hello_spring.controller;
 
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -18,12 +20,12 @@ public class MemberController {
     }
 
     @GetMapping("/members/new")
-    public String createForm(){
+    public String createForm() {
         return "members/createMemberForm";
     }
 
-    @PatchMapping("/members/new")
-    public String create(MemberForm form){
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
         Member member = new Member();
         member.setName(form.getName());
 
@@ -31,4 +33,12 @@ public class MemberController {
 
         return "redirect:/";
     }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
 }
