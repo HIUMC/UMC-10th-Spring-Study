28724711f283d91ea283159295f63ffbf7896a75
노트북 <sun010331@naver.com>
2026-03-25 20:56:51 +0900

    feat : 스프링 빈과 예제

--- sun010331/spring-hello/src/main/java/hello/spring_hello/aop/TimeTraceApp.java
+++ sun010331/spring-hello/src/main/java/hello/spring_hello/aop/TimeTraceApp.java
@@ -1,4 +1,23 @@
 package hello.spring_hello.aop;
 
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
 public class TimeTraceApp {
+
+    public Object execute (ProceedingJoinPoint joinPoint) throws  Throwable{
+
+        long start = System.currentTimeMillis();
+        try{
+            Object result = joinPoint.proceed();
+            return result;
+        } finally{
+            long finish = System.currentTimeMillis();
+            long timeMs = finish  - start;
+        }
+        System.out.println("START: " + joinPoint.toString());
+
+    }
 }
--- sun010331/spring-hello/src/main/java/hello/spring_hello/controller/MemberForm.java
+++ sun010331/spring-hello/src/main/java/hello/spring_hello/controller/MemberForm.java
@@ -1,4 +1,16 @@
 package hello.spring_hello.controller;
 
+import hello.spring_hello.domain.Member;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
 public class MemberForm {
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
