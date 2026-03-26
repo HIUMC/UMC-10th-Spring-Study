5a56ace2e62d71fa68de2b715e8266e22c44434e
Yujin Kim <yujii22001@gmail.com>
2026-03-26 13:23:05 +0900

    Feat(김유진(유자)): 섹션8. AOP

--- sophia22001/스프링 입문/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ sophia22001/스프링 입문/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -26,11 +26,12 @@ public class MemberService {
      * 회원 가입
      */
     public Long join(Member member) {
+
         // 같은 이름의 중복 회원은 안된다. (요구사항)
         validateDuplicateMember(member);
-
         memberRepository.save(member);
         return member.getId();
+
     }
 
     private void validateDuplicateMember(Member member) {
