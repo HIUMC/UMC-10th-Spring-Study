79bd72f0fad04a03275142ea6383847bfb04c59f
박서영 <emilypark0721@gmail.com>
2026-03-29 16:25:40 +0900

    [Feat(박서영(조이): 회원 도메인 기능 추가]

---
+++ Mymyseoyoung/스프링 핵심 원리 - 기본편/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService {
+
+    //인터페이스만 가지고 있으면 오류남 !
+    // 그러므로 구현체인 MemoryMemberRepository 끌고오기
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
