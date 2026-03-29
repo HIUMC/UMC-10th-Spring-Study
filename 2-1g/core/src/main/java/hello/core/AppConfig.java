345d299523474b32fe3dc3a17124129903e73c62
21g <leehan0311@gmail.com>
2026-03-29 17:11:46 +0900

    [Feat(leehan): 객체를 DI컨테이너에 Bean으로 등록]
    AppConfig 파일에서 @Configuration 과 @Bean 어노테이션을 사용해 객체를 Bean에 등록함, ApplicationContext를 이용해 객체를 꺼내줌.

--- 2-1g/core/src/main/java/hello/core/AppConfig.java
+++ 2-1g/core/src/main/java/hello/core/AppConfig.java
@@ -9,27 +9,38 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /// DIP, OCP 원칙을 지키기 위해 의존성을 주입하는 별도의 파일이 있어야 함
 /// 즉, 서비스 파일에 구현체를 주입하는 별도의 설정 파일
 /// 애플리케이션 동작에 필요한 구현 객체를 생성, 생성자를 통해 주입 - DI
+
+@Configuration
 public class AppConfig {
 
+    // Bean 어노테이션을 통해 스프링 컨테이너 빈에 인스턴스를 등록
+    // Bean은 기본적으로 Bean이 붙은 메서드의 이름을 이름으로 한다
+    // 또는 @Bean("leehan") 과 같이 이름을 지어줄 수 있다
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private static MemberRepository memberRepository() {
+    @Bean
+    public static MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     // 만약 할인 정책을 바꾸려면, 이 부분만 고치면 됨
     // OCP 원칙이 매우 잘 지켜짐
-    private static DiscountPolicy discountPolicy() {
+    @Bean
+    public static DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
 }
