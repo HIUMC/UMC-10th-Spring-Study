2a390eb7ac7e6fda26300cdde205580e78d5781b
Yujin Kim <yujii22001@gmail.com>
2026-03-29 22:44:39 +0900

    [Feat(김유진(유자)): 섹션4. 스프링 핵심 원리 이해2 - 객체 지향 원리 적용]

---
+++ sophia22001/스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,40 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // @Bean을 모두 스프링 컨테이너에 등록한다. 메서드 이름이 스프링 빈 이름이 된다.
+public class AppConfig {
+
+    // 역할과 구현이 확실히 분리된다.
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+}
