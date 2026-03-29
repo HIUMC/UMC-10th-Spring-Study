50a7e21e8324785322c6860e962a7372976c72bb
Yoonsung Nam <juju453@naver.com>
2026-03-29 23:22:18 +0900

    [Feat(남윤성(성남)): 섹션 3 완료]

---
+++ skays1212/spring-basic/src/main/java/hello/spring_basic/AppConfig.java
@@ -0,0 +1,37 @@
+package hello.spring_basic;
+
+import hello.spring_basic.discount.DiscountPolicy;
+import hello.spring_basic.discount.FixDiscountPolicy;
+import hello.spring_basic.discount.RateDiscountPolicy;
+import hello.spring_basic.member.MemberService;
+import hello.spring_basic.member.MemberServiceImpl;
+import hello.spring_basic.member.MemoryMemberRepository;
+import hello.spring_basic.order.OrderService;
+import hello.spring_basic.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    @Bean
+    public MemoryMemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+}
--- skays1212/spring-basic/src/main/java/hello/spring_basic/order/OrderApp.java
+++ skays1212/spring-basic/src/main/java/hello/spring_basic/order/OrderApp.java
@@ -1,22 +1,32 @@
 package hello.spring_basic.order;
 
+import hello.spring_basic.AppConfig;
 import hello.spring_basic.member.Grade;
 import hello.spring_basic.member.Member;
 import hello.spring_basic.member.MemberService;
 import hello.spring_basic.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
 
         System.out.println("order = " + order);
         System.out.println("order.calculatePrice = " + order.calculatePrice());
