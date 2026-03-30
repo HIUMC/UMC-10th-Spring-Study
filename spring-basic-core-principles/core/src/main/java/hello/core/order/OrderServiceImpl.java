b91465bc7eaf32d93152db03a2bad44f1970d240
j2nooh <wisestorm01@gmail.com>
2026-03-30 15:34:48 +0900

    feat: 주문과 할인 도메인 실행 및 테스트

--- spring-basic-core-principles/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring-basic-core-principles/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -10,7 +10,7 @@ public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
     private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
 
