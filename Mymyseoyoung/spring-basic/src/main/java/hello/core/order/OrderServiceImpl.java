5bff6ba687d81c21fd5a8126a4fca49330cc4ef8
박서영 <emilypark0721@gmail.com>
2026-03-30 19:44:00 +0900

    [Feat(박서영(조이)):OrderServiceImpl에 DIP 적용 및 DiscountPolicy 인터페이스 의존으로 변경]

--- Mymyseoyoung/spring-basic/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Mymyseoyoung/spring-basic/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,18 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository=new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy=new FixDiscountPolicy();
+    //구체적인 코드를 의존하고 있음 .
+   // private final DiscountPolicy discountPolicy=new FixDiscountPolicy();
+
+    //private final DiscountPolicy discountPolicy=new RateDiscountPolicy();
+
+    // DIP 위반 해결방법
+    //인터페이스에만 의존함
+    //-> NullPointException이 생김 !
+
+    private DiscountPolicy discountPolicy;
+    //해결방법 -> 누군가가 클라이언트인 OrderServiceImpl에  DiscountPolicy의 구현객체를
+    //대신 생성하고 주입해주어야 함
 
     @Override
     public Order createOrder(Long memberId,String itemName,int itemPrice) {
