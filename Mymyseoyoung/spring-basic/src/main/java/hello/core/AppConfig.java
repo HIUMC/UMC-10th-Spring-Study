47cf1dbff9ec0300eed4e797b28ad7da01019a9f
박서영 <emilypark0721@gmail.com>
2026-03-30 22:04:12 +0900

    [Refactor(박서영(조이)): AppConfig 리팩터링]

--- Mymyseoyoung/spring-basic/src/main/java/hello/core/AppConfig.java
+++ Mymyseoyoung/spring-basic/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -11,11 +13,22 @@ public class AppConfig {
     public MemberService memberService()
     {
         //메모리 멤버 리포지토리 자동 호출
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        //아래에 어떤 리포지토리가 호출되는지도 다 나와있게 !!
+        return new MemberServiceImpl(memberRepository());
     }
 
+    //나중에 DB 관련으로 바꾸고 싶으면 얘만 바꿔주면 됨 !
+    public MemberRepository memberRepository()
+    {
+        return new MemoryMemberRepository();
+    }
     public OrderService orderService()
     {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(new MemoryMemberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy()
+    {
+        return new FixDiscountPolicy();
     }
 }
