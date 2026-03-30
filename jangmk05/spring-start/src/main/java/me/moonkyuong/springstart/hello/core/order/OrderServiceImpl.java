39ef14e18fd9b68d2be9b17b7b670929bd65e540
jangmk05 <jangmk05@gmail.com>
2026-03-30 11:00:20 +0900

    [Refactor(Ellie)] : 할인 정책 변경 적용

--- jangmk05/spring-start/src/main/java/me/moonkyuong/springstart/hello/core/order/OrderServiceImpl.java
+++ jangmk05/spring-start/src/main/java/me/moonkyuong/springstart/hello/core/order/OrderServiceImpl.java
@@ -2,13 +2,16 @@ package me.moonkyuong.springstart.hello.core.order;
 
 import me.moonkyuong.springstart.hello.core.discount.DiscountPolicy;
 import me.moonkyuong.springstart.hello.core.discount.FixDiscountPolicy;
+import me.moonkyuong.springstart.hello.core.discount.RateDiscountPolicy;
 import me.moonkyuong.springstart.hello.core.member.Member;
 import me.moonkyuong.springstart.hello.core.member.MemberRepository;
 import me.moonkyuong.springstart.hello.core.member.MemoryMemberRespository;
 
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository = new MemoryMemberRespository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
