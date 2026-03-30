f8ec5a808554c19ca3ba0913f05ed9b697b03700
jangmk05 <jangmk05@gmail.com>
2026-03-30 11:14:49 +0900

    [Refactor(Ellie)] : 스프링으로 전환

--- jangmk05/spring-start/src/main/java/me/moonkyuong/springstart/hello/core/AppConfig.java
+++ jangmk05/spring-start/src/main/java/me/moonkyuong/springstart/hello/core/AppConfig.java
@@ -1,7 +1,6 @@
 package me.moonkyuong.springstart.hello.core;
 
 import me.moonkyuong.springstart.hello.core.discount.DiscountPolicy;
-import me.moonkyuong.springstart.hello.core.discount.FixDiscountPolicy;
 import me.moonkyuong.springstart.hello.core.discount.RateDiscountPolicy;
 import me.moonkyuong.springstart.hello.core.member.MemberRepository;
 import me.moonkyuong.springstart.hello.core.member.MemberService;
@@ -9,21 +8,25 @@ import me.moonkyuong.springstart.hello.core.member.MemberServiceImpl;
 import me.moonkyuong.springstart.hello.core.member.MemoryMemberRespository;
 import me.moonkyuong.springstart.hello.core.order.OrderService;
 import me.moonkyuong.springstart.hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
 
 public class AppConfig {
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy());
     }
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRespository();
     }
+    @Bean
     public DiscountPolicy discountPolicy() {
-        // return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
 }
