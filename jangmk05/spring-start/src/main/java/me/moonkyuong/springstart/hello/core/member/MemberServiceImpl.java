67468779278204bfdef446559ca4c71b2f4cc6a8
jangmk05 <jangmk05@gmail.com>
2026-03-30 10:17:48 +0900

    [Feat(Ellie)] : 회원 도메인 개발

---
+++ jangmk05/spring-start/src/main/java/me/moonkyuong/springstart/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,11 @@
+package me.moonkyuong.springstart.hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+    private final MemberRepository memberRepository = new MemoryMemberRespository();
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
