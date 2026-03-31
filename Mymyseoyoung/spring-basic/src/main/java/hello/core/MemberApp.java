e6f5de436604feadd8ebec2b7d418ecefe9abcf6
박서영 <emilypark0721@gmail.com>
2026-03-31 13:40:36 +0900

    [Feat(박서영(조이)): Application Context 추가]

--- Mymyseoyoung/spring-basic/src/main/java/hello/core/MemberApp.java
+++ Mymyseoyoung/spring-basic/src/main/java/hello/core/MemberApp.java
@@ -5,17 +5,26 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService=appConfig.memberService();
+
+
+
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService=appConfig.memberService();
         //멤버 서비스 구현체를 만들고
         // -> 내가 만든 멤버 서비스 구현체는 메모리멤버 리포지토리를 사용한다고 했음 .
 
 
       //  MemberService memberService = new MemberServiceImpl();
+
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
         Member member = new Member(1L,"memberA", Grade.VIP);
         memberService.join(member);
 
