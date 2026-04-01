939371d90a7847676e5de32b41f4c98535b562aa
9dongs <144160177+9dongs@users.noreply.github.com>
2026-04-02 03:10:48 +0900

    [Refactor(구디) : Appconfig 리팩터링]

--- 9dongs/스프링 핵심 원리/src/main/java/hello/core/Appconfig.java
+++ 9dongs/스프링 핵심 원리/src/main/java/hello/core/Appconfig.java
@@ -1,11 +1,13 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.jspecify.annotations.NonNull;
 
 // 앱 실제 동작에 필요한 구현 객체 생성
 // 생성자를 통해 의존성 주입
@@ -13,11 +15,19 @@ public class Appconfig {
 
     // 생성자 주입
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static @NonNull MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
