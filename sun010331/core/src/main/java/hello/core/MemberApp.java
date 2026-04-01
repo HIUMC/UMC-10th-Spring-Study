1e49137d79a5f77ec43c25786589bfc08f9afaa6
노트북 <sun010331@naver.com>
2026-04-01 17:49:10 +0900

    feat : 섹션 4 전체흐름정리

--- sun010331/core/src/main/java/hello/core/MemberApp.java
+++ sun010331/core/src/main/java/hello/core/MemberApp.java
@@ -8,11 +8,13 @@ import hello.core.member.MemberServiceImpl;
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        Member memberA = new Member(1L, "memberA", Grade.VIP);
-        memberService.join(memberA);
+       AppConfig appConfig =new AppConfig();
+       MemberService memberServicer = appConfig.memberService();
 
-        Member findMember = memberService.findMember(1L);
+        Member memberA = new Member(1L, "memberA", Grade.VIP);
+        memberServicer.join(memberA);
+
+        Member findMember = memberServicer.findMember(1L);
 
         System.out.println("new Member = " + memberA.getName());
         System.out.println("findMember = " + findMember.getName());
--- sun010331/core/src/main/java/hello/core/OrderApp.java
+++ sun010331/core/src/main/java/hello/core/OrderApp.java
@@ -3,17 +3,18 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+
 import hello.core.order.Order;
 import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
+
 
 public class OrderApp {
 
     public static void main(String[] args) {
+        AppConfig appConfig = new AppConfig();
 
-        MemberService service = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        MemberService service = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member a = new Member(1L, "a", Grade.VIP);
