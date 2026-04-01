084bf9a6da2d66853e1aa2d3e73494a104a62037
minjiwon <minwldnjs87@gmail.com>
2026-04-01 16:47:21 +0900

    [Feat(민지원(나우)): 스프링 전환]

--- 스프링 핵심 원리/core/src/main/java/hello/core/MemberApp.java
+++ 스프링 핵심 원리/core/src/main/java/hello/core/MemberApp.java
@@ -5,13 +5,19 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.order.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService(); //memberServiceImpl이 들어가있음
+        //       AppConfig appConfig = new AppConfig();
+    //        MemberService memberService = appConfig.memberService(); //memberServiceImpl이 들어가있음
     //    MemberService memberService = new MemberServiceImpl();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
--- 스프링 핵심 원리/core/src/main/java/hello/core/order/AppConfig.java
+++ 스프링 핵심 원리/core/src/main/java/hello/core/order/AppConfig.java
@@ -7,27 +7,32 @@ import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import io.micrometer.observation.ObservationRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     //매서드 명을 통해 역할이 다 드러남.
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository()); // 생성자 주입..?
     }
 
-    private MemberRepository memberRepository(){
+    @Bean
+    public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new FixDiscountPolicy();
     }
-
-
-
+    
 
 }
