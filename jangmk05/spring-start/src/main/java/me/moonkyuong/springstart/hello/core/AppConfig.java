90987255e11e8a285b2390b20854e6c6031d455a
jangmk05 <jangmk05@gmail.com>
2026-04-05 01:45:13 +0900

    [Feat(Ellie)] : 각 객체별 호출 로그 추가

--- jangmk05/spring-start/src/main/java/me/moonkyuong/springstart/hello/core/AppConfig.java
+++ jangmk05/spring-start/src/main/java/me/moonkyuong/springstart/hello/core/AppConfig.java
@@ -9,20 +9,27 @@ import me.moonkyuong.springstart.hello.core.member.MemoryMemberRepository;
 import me.moonkyuong.springstart.hello.core.order.OrderService;
 import me.moonkyuong.springstart.hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
+
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy());
     }
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
+
         return new MemoryMemberRepository();
     }
     @Bean
