4e32eb28d33f77404ce6f3344fdb482f9bc92cef
cyunwook <cyunwook@gmail.com>
2026-04-05 00:21:00 +0900

    섹션 6완료

--- cyunwook/스프링 핵심원리/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ cyunwook/스프링 핵심원리/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -11,4 +11,5 @@ public class MemoryMemberRepository implements MemberRepository {
     public Member findById(Long memberId) {
         return store.get(memberId);
     }
+
 }
--- cyunwook/스프링 핵심원리/src/main/java/hello/core/order/OrderServiceImpl.java
+++ cyunwook/스프링 핵심원리/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -21,4 +21,9 @@ public class OrderServiceImpl implements OrderService {
 
         return new Order(memberId, itemName, itemPrice, DiscountPrice);
     }
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
+
+
 }
