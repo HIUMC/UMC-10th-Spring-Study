f9609485e4b8597b6f0127ee2f57c2b43452fbec
jangmk05 <jangmk05@gmail.com>
2026-04-05 01:19:35 +0900

    [Test(Ellie)] : 싱글톤 패턴 주의사항 테스트

---
+++ jangmk05/spring-start/src/test/java/me/moonkyuong/springstart/hello/core/singleton/StatefulService.java
@@ -0,0 +1,14 @@
+package me.moonkyuong.springstart.hello.core.singleton;
+
+public class StatefulService {
+    private int price;
+
+    public void order(String name, int price) {
+        System.out.println("name = " + name + "price = " + price);
+        this.price=price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
