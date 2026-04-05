718fb118b06930ff033988bedd7373bdfb6027f7
jangmk05 <jangmk05@gmail.com>
2026-04-05 13:55:02 +0900

    [Feat(Ellie)] : 컴포넌트 스캔 & 의존관계 자동 주입 테스트

---
+++ jangmk05/spring-start/src/test/java/me/moonkyuong/springstart/hello/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,18 @@
+package me.moonkyuong.springstart.hello.core.scan;
+
+import me.moonkyuong.springstart.hello.core.AutoAppConfig;
+import me.moonkyuong.springstart.hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class AutoAppConfigTest {
+    @Test
+    void basicScan(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
