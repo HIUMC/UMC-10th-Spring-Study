d4e31ddba4bbb1cdfb3ad0db882220731b97e63e
jangmk05 <jangmk05@gmail.com>
2026-04-05 23:54:01 +0900

    [Chore(Ellie)] : 프로젝트 초기세팅

---
+++ jangmk05/demo/src/main/java/com/example/demo/domain/mission/entity/mapping/MemberMission.java
@@ -0,0 +1,4 @@
+package com.example.demo.domain.mission.entity.mapping;
+
+public class MemberMission {
+}
---
+++ jangmk05/demo/src/main/java/com/example/demo/domain/mission/exception/code/MissionSuccessCode.java
@@ -0,0 +1,4 @@
+package com.example.demo.domain.mission.exception.code;
+
+public enum MissionSuccessCode {
+}
