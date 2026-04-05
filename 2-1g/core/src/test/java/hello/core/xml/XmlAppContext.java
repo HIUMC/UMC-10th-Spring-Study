b90926ff960c7c6cb211b8e84db911d3bbcc9f42
21g <leehan0311@gmail.com>
2026-04-05 15:10:55 +0900

    [Feat(leehan): 다양한 설정 형식으로 Bean 등록하기, BeanDefinition 확인하기]

---
+++ 2-1g/core/src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,19 @@
+package hello.core.xml;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+// bean 설정은 Configuration파일 뿐만 아니라 xml파일로도 가능하다
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
