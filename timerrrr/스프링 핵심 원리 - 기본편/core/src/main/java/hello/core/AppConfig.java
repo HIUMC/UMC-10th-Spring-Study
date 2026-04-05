229535929e54678e37d03d7d593948fff0529a37
[timerrrr] <[sichan0304@naver.com]>
2026-04-05 21:19:16 +0900

    [Feat(송시찬(시치)): 섹션6. 싱글톤 컨테이너]

--- timerrrr/스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AppConfig.java
+++ timerrrr/스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AppConfig.java
@@ -11,22 +11,26 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.SimpleRouteMatcher;
 
 @Configuration
 public class AppConfig {
 
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
-    public static MemberRepository memberRepository() {
+    public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
