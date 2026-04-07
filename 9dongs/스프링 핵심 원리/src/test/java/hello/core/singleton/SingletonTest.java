ed5c53c51dbcab88e58d0a43d843d245529a4472
9dongs <144160177+9dongs@users.noreply.github.com>
2026-04-07 14:45:09 +0900

    [Feat(구디) : (싱글톤) 스프링 컨테이너 사용해 테스트 구현]

--- 9dongs/스프링 핵심 원리/src/test/java/hello/core/singleton/SingletonTest.java
+++ 9dongs/스프링 핵심 원리/src/test/java/hello/core/singleton/SingletonTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.assertj.core.api.ObjectAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -42,5 +44,25 @@ public class SingletonTest {
         assertThat(singletonService1).isSameAs(singletonService2);
         // same : ==
         // equal : equals라는 메서드 오버라이드
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+//        Appconfig appConfig = new Appconfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(Appconfig.class);
+
+        // 1. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService1 = ac.getBean("memberService",MemberService.class);
+
+        // 2. 조회 : 호추랗ㄹ 때 마다 객체를 생성
+        MemberService memberService2 = ac.getBean("memberService",MemberService.class);
+
+        // 참조 값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 != memberService2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
