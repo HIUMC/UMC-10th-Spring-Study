375c1f6198af2452e0105d0a4b590cd3ad1c90f8
jangmk05 <jangmk05@gmail.com>
2026-04-27 19:33:44 +0900

    [Feat(Ellie)] : 인터페이스 InitializingBean, DisposableBean

--- jangmk05/spring-start/src/main/java/me/moonkyuong/springstart/hello/core/lifecycle/NetworkClient.java
+++ jangmk05/spring-start/src/main/java/me/moonkyuong/springstart/hello/core/lifecycle/NetworkClient.java
@@ -1,7 +1,21 @@
 package me.moonkyuong.springstart.hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
+
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
