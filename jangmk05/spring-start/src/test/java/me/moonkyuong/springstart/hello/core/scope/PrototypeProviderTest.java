2899e429a5ecfacf59f5658559b59e32f8b3ad2d
jangmk05 <jangmk05@gmail.com>
2026-04-27 20:53:28 +0900

    [Test(Ellie)] : ObjectProvider 사용(프로토타입 스코프 문제 해결1)

--- jangmk05/spring-start/src/test/java/me/moonkyuong/springstart/hello/core/scope/PrototypeProviderTest.java
+++ jangmk05/spring-start/src/test/java/me/moonkyuong/springstart/hello/core/scope/PrototypeProviderTest.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -26,10 +27,10 @@ public class PrototypeProviderTest {
     static class ClientBean {
 
         @Autowired
-        private ApplicationContext ac;
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
-            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
