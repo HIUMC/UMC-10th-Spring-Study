d7ab551fa6ff415ba5ed6d7bed402d7f1215c938
Yujin Kim <yujii22001@gmail.com>
2026-04-27 22:19:28 +0900

    [Feat(김유진(유자)): 섹션 8. 의존관계 자동 주입]

--- sophia22001/스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AutoAppConfig.java
+++ sophia22001/스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AutoAppConfig.java
@@ -11,8 +11,10 @@ import org.springframework.context.annotation.FilterType;
 @ComponentScan( // @Component가 붙은 클래스를 스프링 빈으로 등록한다.
         // @Configuration안에 @Component가 있기 때문에 스캔되지 않도록 제외시킨다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class),
-        basePackages = "hello.core.member",
-        basePackageClasses = AutoAppConfig.class
+        basePackages = "hello.core.member", // 이 문자열의 패키지 기준으로 탐색
+        basePackageClasses = AutoAppConfig.class // 이 클래스가 위치한 패키지를 기준으로 탐색
+
+        // basePackages와 basePackageClasses 둘다 설정하면, 두 범위가 함쳐진 범위로 적용된다.
 
 )
 public class AutoAppConfig {
