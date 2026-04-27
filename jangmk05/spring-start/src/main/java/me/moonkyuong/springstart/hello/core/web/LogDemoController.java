ce76091d5c48cabf69b99d27b32eee9099912844
jangmk05 <jangmk05@gmail.com>
2026-04-27 21:42:55 +0900

    [Refactor(Ellie)] : 프록시 사용

--- jangmk05/spring-start/src/main/java/me/moonkyuong/springstart/hello/core/web/LogDemoController.java
+++ jangmk05/spring-start/src/main/java/me/moonkyuong/springstart/hello/core/web/LogDemoController.java
@@ -4,7 +4,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import me.moonkyuong.springstart.hello.core.common.MyLogger;
 import me.moonkyuong.springstart.hello.core.logdemo.LogDemoService;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,13 +12,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
         logDemoService.logic("testId");
