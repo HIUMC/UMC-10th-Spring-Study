bf7b3fdf5b9f8d2996b9b8cf07ef5fec14189a7a
9dongs <144160177+9dongs@users.noreply.github.com>
2026-04-29 03:12:29 +0900

    [Refactor(구디) : 롬복 사용해 코드 간결화]

--- 9dongs/스프링 핵심 원리/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 9dongs/스프링 핵심 원리/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,22 +3,24 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // 필수값 (final) 를 파라미터로 받는 생성자를 만들어줌
 public class OrderServiceImpl implements OrderService {
 
     // final -> 초기값 넣어줘야된다고 알려줌
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-
-    @Autowired // 생략 가능
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//
+//    @Autowired // 생략 가능
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
