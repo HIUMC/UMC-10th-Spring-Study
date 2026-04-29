f0fbc7330a6a1b3263681061b9b90d57041b0ad0
21g <leehan0311@gmail.com>
2026-04-29 09:32:03 +0900

    [Feat(leehan): Scope를 request로 변경하여 Log 설정하기]

---
+++ 2-1g/core/src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,39 @@
+package hello.core.common;
+
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@Scope(value = "request")
+// HTTP 요청 당 하나씩 생성, 요청이 끝나는 시점에 소멸된다
+// uuid가 있기 때문에 다른 HTTP 요청과 구분할 수 있다.
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "]" + message);
+    }
+
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create:" + this);
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println();
+        System.out.println("[" + uuid + "] request scope bean close:" + this);
+    }
+}
