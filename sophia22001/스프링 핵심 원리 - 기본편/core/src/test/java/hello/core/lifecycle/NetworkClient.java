652c088cb77f2f4c179ba3424d38c945f31e2f2c
Yujin Kim <yujii22001@gmail.com>
2026-04-29 15:10:56 +0900

    [Feat(김유진(유자)): 섹션 9. 빈 생명주기 콜백-애노테이션]

--- sophia22001/스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ sophia22001/스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,10 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
 public class NetworkClient {
 
     private String url;
@@ -20,11 +25,26 @@ public class NetworkClient {
     }
 
     public void call(String message) {
-        System.out.println("call = " + url + "message = " + message);
+        System.out.println("call = " + url + ", message = " + message);
     }
 
     // 서비스 종료 시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    // 의존관계 끝나고 호출됨
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // 빈이 종료될 때 호출됨
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
     }
 }
