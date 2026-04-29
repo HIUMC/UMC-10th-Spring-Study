4f0341bccd5ae52114d76d890f5cd036dad0deb6
9dongs <144160177+9dongs@users.noreply.github.com>
2026-04-30 02:41:49 +0900

    [Feat(구디) : 싱글톤 빈/프로토타입 스코프와 함께 사용시 문제 provider로 해결]

--- 9dongs/스프링 핵심 원리/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ 9dongs/스프링 핵심 원리/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,7 +2,9 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -34,19 +36,37 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.login();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입
 
+        // 싱글톤 빈이 프로토타입을 사용할 때 마다 스프링 컨테이너에 새로 요청
+//        @Autowired private ApplicationContext ac;
+//        public int logic() {
+//            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
+//            prototypeBean.addCount();
+//            int count = prototypeBean.getCount();
+//            return count;
+//        }
+
+        // ObjectProvider
+//        @Autowired
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+//
+//        public int login() {
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject(); // 새로운 프로토타입 빈 생성, 대신 조회자(대리자)
+//            prototypeBean.addCount();
+//            return prototypeBean.getCount();
+//        }
+
+        // JSR-330 Provider
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int login() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get(); // 새로운 프로토타입 빈 생성, 대신 조회자(대리자)
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
