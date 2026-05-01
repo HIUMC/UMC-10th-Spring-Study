6e5923f0e0d3fb1e3ce8f372b543c04dbd25f6a7
21g <leehan0311@gmail.com>
2026-05-01 22:35:39 +0900

    [Feat(leehan): jpashop 프로젝트 환경설정 및 기본적인 파일 생성, DB 동작 확인]

--- 2-1g/core/src/main/java/hello/core/common/MyLogger.java
+++ 2-1g/core/src/main/java/hello/core/common/MyLogger.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 import java.util.UUID;
 
 @Component
+// 일단 가짜 프록시 객체를 주입하고, 필요한 시점에 진짜를 찾아서 주입해줌
 @Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 // HTTP 요청 당 하나씩 생성, 요청이 끝나는 시점에 소멸된다
 // uuid가 있기 때문에 다른 HTTP 요청과 구분할 수 있다.
