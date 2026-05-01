565c8963cf4c04d72f690b4977ca7187bb9b5982
Yujin Kim <yujii22001@gmail.com>
2026-05-01 18:36:01 +0900

    [Feat(김유진(유자)): 에러 핸들링 기본 예제]

--- sophia22001/mission4/src/main/java/com/example/mission4/domain/member/controller/MemberController.java
+++ sophia22001/mission4/src/main/java/com/example/mission4/domain/member/controller/MemberController.java
@@ -1,4 +1,18 @@
 package com.example.mission4.domain.member.controller;
 
+import com.example.mission4.domain.member.exception.MemberException;
+import com.example.mission4.domain.member.exception.code.MemberErrorCode;
+import com.example.mission4.global.apiPayload.ApiResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MemberController {
+
+    // 예시 요청 (멤버를 찾을 수 없음)
+    @GetMapping("/test")
+    public ApiResponse<String> test() {
+        // throw new Exception("test");
+        throw new MemberException(MemberErrorCode.MEMBER_NOT_FOUND);
+    }
 }
