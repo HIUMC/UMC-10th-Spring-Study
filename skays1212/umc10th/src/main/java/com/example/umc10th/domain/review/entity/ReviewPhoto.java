2ac076f537e39da0a3944093e74bf98ceffeba5c
Yoonsung Nam <juju453@naver.com>
2026-05-05 04:31:30 +0900

    [Feat(성남): 리뷰 관련 엔티티 구현]

--- skays1212/umc10th/src/main/java/com/example/umc10th/domain/review/entity/ReviewPhoto.java
+++ skays1212/umc10th/src/main/java/com/example/umc10th/domain/review/entity/ReviewPhoto.java
@@ -1,4 +1,26 @@
 package com.example.umc10th.domain.review.entity;
 
-public class ReviewPhoto {
+import com.example.umc10th.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "review_photo")
+public class ReviewPhoto extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_photo_id")
+    private Long id;
+
+    @Column(name = "photo_url", nullable = false)
+    private String photoUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id", nullable = false)
+    private Review review;
 }
