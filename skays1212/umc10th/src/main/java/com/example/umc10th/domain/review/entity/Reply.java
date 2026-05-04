5cb1c4be592e68eb8d5771f1712b4d1ed1fb5e99
Yoonsung Nam <juju453@naver.com>
2026-05-05 04:31:25 +0900

    [Feat(성남): 리뷰 관련 엔티티 구현]

--- skays1212/umc10th/src/main/java/com/example/umc10th/domain/review/entity/Reply.java
+++ skays1212/umc10th/src/main/java/com/example/umc10th/domain/review/entity/Reply.java
@@ -1,4 +1,25 @@
 package com.example.umc10th.domain.review.entity;
 
-public class Reply {
+import com.example.umc10th.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "reply")
+public class Reply extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reply_id")
+    private Long id;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @OneToOne(mappedBy = "reply", fetch = FetchType.LAZY)
+    private Review review;
 }
