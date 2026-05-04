1e7a0015ceebfc8e62724283ffdabd2fb6f9aed8
Yoonsung Nam <juju453@naver.com>
2026-05-05 04:22:35 +0900

    [Feat(성남): 지역 엔티티 구현]

--- skays1212/umc10th/src/main/java/com/example/umc10th/domain/mission/entity/Location.java
+++ skays1212/umc10th/src/main/java/com/example/umc10th/domain/mission/entity/Location.java
@@ -1,4 +1,24 @@
 package com.example.umc10th.domain.mission.entity;
 
-public class Location {
+import com.example.umc10th.domain.mission.enums.Address;
+import com.example.umc10th.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "location")
+public class Location extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "location_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private Address name;
 }
