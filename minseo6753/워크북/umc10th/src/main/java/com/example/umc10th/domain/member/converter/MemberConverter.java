9c0600791acc17b08a37b9750825bc1b5db2921f
minseo6753 <minseo6753@naver.com>
2026-05-07 06:04:32 +0900

    [Feat(minseo6753/워크북): 마이페이지 조회 API 구현

--- minseo6753/워크북/umc10th/src/main/java/com/example/umc10th/domain/member/converter/MemberConverter.java
+++ minseo6753/워크북/umc10th/src/main/java/com/example/umc10th/domain/member/converter/MemberConverter.java
@@ -1,4 +1,16 @@
 package com.example.umc10th.domain.member.converter;
 
+import com.example.umc10th.domain.member.dto.MemberResDTO;
+import com.example.umc10th.domain.member.entity.Member;
+
 public class MemberConverter {
+
+    public static MemberResDTO.MyPage toMyPage(Member member) {
+        return MemberResDTO.MyPage.builder()
+                .nickname(member.getNickname())
+                .email(member.getEmail())
+                .phoneNumber(member.getPhoneNumber())
+                .point(member.getPoint())
+                .build();
+    }
 }
