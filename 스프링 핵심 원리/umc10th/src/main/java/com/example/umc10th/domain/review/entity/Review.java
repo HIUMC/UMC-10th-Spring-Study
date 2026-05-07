bdfc0b4cf2628bb68da2195962f0798037e02f03
minjiwon <minwldnjs87@gmail.com>
2026-05-07 23:01:46 +0900

    feat: 리뷰 도메인 엔티티 및 연관관계 매핑 추가

--- 스프링 핵심 원리/umc10th/src/main/java/com/example/umc10th/domain/review/entity/Review.java
+++ 스프링 핵심 원리/umc10th/src/main/java/com/example/umc10th/domain/review/entity/Review.java
@@ -1,4 +1,45 @@
 package com.example.umc10th.domain.review.entity;
 
-public class Review {
+import com.example.umc10th.domain.member.entity.Member;
+import com.example.umc10th.domain.mission.entity.Store;
+import com.example.umc10th.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "review")
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private BigDecimal star;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    @OneToOne(mappedBy = "review", cascade = CascadeType.ALL)
+    private Reply reply;
+
+    @OneToMany(mappedBy = "review", cascade = CascadeType.ALL)
+    @Builder.Default
+    private List<ReviewPhoto> reviewPhotoList = new ArrayList<>();
 }
