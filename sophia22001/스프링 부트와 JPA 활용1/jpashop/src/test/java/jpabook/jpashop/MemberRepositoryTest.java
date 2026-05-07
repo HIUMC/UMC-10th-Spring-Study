dd94456c0e3ccaec1c371958a7879227ab65a4aa
Yujin Kim <yujii22001@gmail.com>
2026-05-08 00:45:20 +0900

    [Feat(김유진(유자)): 섹션 2. JPA와 DB 설정]

---
+++ sophia22001/스프링 부트와 JPA 활용1/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional // Entity Manager에게는 꼭 필요하다.
+    @Rollback(false)
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
+        Assertions.assertThat(findMember).isEqualTo(member); // JPA 엔티티 동일성 보장 // 같은 영속성 컨텍스트 안에서는 식별자가 같으면 같은 엔티티
+
+
+    }
+
+}
