b99b649624e02a88b897d460d4bab96340661fb7
Cuyqo <saon0410@gmail.com>
2026-05-10 22:30:27 +0900

    [Feat(choiii)] 6주차 미션 Query 수정

--- choiii/week5/umc10th/src/main/java/com/example/umc10th/domain/mission/repository/UserMissionRepository.java
+++ choiii/week5/umc10th/src/main/java/com/example/umc10th/domain/mission/repository/UserMissionRepository.java
@@ -6,12 +6,31 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserMissionRepository extends JpaRepository<UserMission, Long> {
-    Page<UserMission> findAllByUserId(Long userId, Pageable pageable);
+    @Query("""
+    SELECT um
+    FROM UserMission um
+    JOIN FETCH um.mission m
+    WHERE um.user.id = :userId
+    """
+        )
+    Page<UserMission> findAllByUserId(
+            @Param("userId") Long userId,
+            Pageable pageable);
+
+    @Query("""
+    SELECT um
+    FROM UserMission um
+    JOIN FETCH um.mission m
+    WHERE um.user.id = :userId
+          AND um.status = :status
+    """)
     Page<UserMission> findAllByUserIdAndStatus(
-            Long userId,
-            MissionStatus status,
+            @Param("userId") Long userId,
+            @Param("status") MissionStatus status,
             Pageable pageable
     );
 }
