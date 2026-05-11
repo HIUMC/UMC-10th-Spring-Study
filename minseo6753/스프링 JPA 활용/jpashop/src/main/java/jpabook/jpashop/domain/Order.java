17e75124f876b130f0204274568dd6c96844f900
minseo6753 <minseo6753@naver.com>
2026-05-12 05:24:06 +0900

    [Feat(minseo6753/스프링 JPA 활용): cascade

--- minseo6753/스프링 JPA 활용/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ minseo6753/스프링 JPA 활용/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -32,10 +33,10 @@ public class Order {
     @JoinColumn(name="member_id")
     private Member member;
 
-    @OneToMany(mappedBy="order")
+    @OneToMany(mappedBy="order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name="delivery_id")
     private Delivery delivery;
 
