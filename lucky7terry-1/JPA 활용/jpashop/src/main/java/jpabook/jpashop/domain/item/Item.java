e27583384163538998c5d70975b09ea20bdb5cf0
terry <rlfox1209@naver.com>
2026-05-11 16:57:59 +0900

    [Feat(김태리(태리)): 섹션 3. 도메인 분석 설계]

--- lucky7terry-1/JPA 활용/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ lucky7terry-1/JPA 활용/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,5 +1,6 @@
-package jpabook.jpashop;
+package jpabook.jpashop.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -8,9 +9,16 @@ import lombok.Setter;
 
 @Entity
 @Getter @Setter
-public class Member {
+public abstract class Item {
+
 
     @Id @GeneratedValue
+    @Column(name = "item_id")
     private Long id;
-    private String username;
+
+    private String name;
+
+    private int price;
+
+    private int stockQuantity;
 }
