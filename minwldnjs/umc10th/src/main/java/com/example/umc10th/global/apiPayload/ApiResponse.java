1264f553c2e4c36a6be88af57cf4f12c6e621e86
minjiwon <minwldnjs87@gmail.com>
2026-05-12 18:20:48 +0900

    fix: minwldnjs 폴더 복구

---
+++ minwldnjs/umc10th/src/main/java/com/example/umc10th/global/apiPayload/ApiResponse.java
@@ -0,0 +1,26 @@
+package com.example.umc10th.global.apiPayload;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+public class ApiResponse<T> {
+
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+    private final String code;
+    private final String message;
+    private final T result;
+
+    public static <T> ApiResponse<T> onSuccess(T result) {
+        return new ApiResponse<>(true, "COMMON200", "요청에 성공하였습니다.", result);
+    }
+
+    public static <T> ApiResponse<T> onFailure(String code, String message, T data) {
+        return new ApiResponse<>(false, code, message, data);
+    }
+}
