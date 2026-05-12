d486961734916e6c680d14a551928abb26c7e3f8
9dongs <144160177+9dongs@users.noreply.github.com>
2026-05-12 10:51:53 +0900

    [Feat(구디) : 엔티티 클래스 개발]

---
+++ 9dongs/JPA 활용/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 상속 관계 전략 지정 ( 싱글 테이블 전략 : 한 테이블에 다 때려 박기 )
+@DiscriminatorColumn(name = "dtype")
+@Getter
+@Setter
+public class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+}
