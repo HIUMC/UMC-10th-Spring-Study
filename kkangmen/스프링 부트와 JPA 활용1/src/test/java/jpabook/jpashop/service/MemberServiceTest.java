c217f3b09476b519d75877afa76ffdd5075cf28c
kkangmen <pickofee@gmail.com>
2026-05-13 11:21:01 +0900

    [찰리/김강민]: 7주차 워크북 제출

---
+++ kkangmen/스프링 부트와 JPA 활용1/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,49 @@
+package jpabook.jpashop.service;
+
+import jakarta.transaction.Transactional;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("kim");
+
+        // when
+        Long savedId = memberService.join(member);
+
+        // then
+        assertThat(member.getId()).isEqualTo(savedId);
+    }
+
+    @Test
+    void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        Member member2 = new Member();
+        member1.setName("kim");
+        member2.setName("kim");
+
+        // when
+        memberService.join(member1);
+
+        // then
+        org.junit.jupiter.api.Assertions.assertThrows(IllegalStateException.class, () -> {
+            memberService.join(member2);
+        });
+    }
+}
