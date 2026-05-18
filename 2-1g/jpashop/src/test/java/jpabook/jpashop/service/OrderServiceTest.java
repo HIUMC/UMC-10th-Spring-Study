3f4efae4593b1366fb5dfaf735de4a824f84c538
21g <leehan0311@gmail.com>
2026-05-18 10:08:22 +0900

    [Feat(leehan): Order, OrderItem 엔티티, 레포지토리, 서비스 구현]

---
+++ 2-1g/jpashop/src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,9 @@
+package jpabook.jpashop.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class OrderServiceTest {
+}
