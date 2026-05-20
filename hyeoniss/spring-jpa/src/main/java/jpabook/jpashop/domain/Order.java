f24e402b258fc63f145ff733969fcba2f8a5f7bd
Kim Ji Hyeon <117696182+Hyeoniss@users.noreply.github.com>
2026-05-20 12:44:34 +0900

    feat: 섹션 7

--- hyeoniss/spring-jpa/src/main/java/jpabook/jpashop/domain/Order.java
+++ hyeoniss/spring-jpa/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,43 +1,36 @@
 package jpabook.jpashop.domain;
 
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.JoinColumnOrFormula;
-
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-
-import static jakarta.persistence.CascadeType.*;
-import static jakarta.persistence.FetchType.*;
-
 @Entity
 @Table(name = "orders")
-@Getter
-@Setter
+@Getter @Setter
 public class Order {
 
-    @Id
-    @GeneratedValue
+    @Id @GeneratedValue
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne(fetch = LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
-    private Member member;
+    private Member member; //주문 회원
 
-    @OneToMany(mappedBy = "order", cascade = ALL) //기본이 LAZY
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = LAZY, cascade = ALL)
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "delivery_id")
-    private Delivery delivery;
 
-    private LocalDateTime orderDate; //주문 시간
+    private Delivery delivery; //배송정보
 
-    @Enumerated(EnumType.STRING) //ORDINAL은 안돼
-    private OrderStatus status; //주문 상태 [ORDER , CANCEL]
+    private LocalDateTime orderDate; //주문시간
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status; //주문상태 [ORDER, CANCEL]
 
     //==연관관계 메서드==//
     public void setMember(Member member) {
@@ -53,5 +46,41 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    //==생성 메서드==// ~ 엔티티 내부에서 처리 ~ 앞으로 변경 시 여기만 바꾸면 ㄱㄴ
+    public static Order createOrder(Member member, Delivery delivery,
+                                    OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    /** 주문 취소 */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+    /** 전체 주문 가격 조회 */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
     }
 }
