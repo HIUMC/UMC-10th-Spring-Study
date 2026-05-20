3b704e072c2cecb8182b2fff51330a04dfc5a805
Yujin Kim <yujii22001@gmail.com>
2026-05-20 17:17:06 +0900

    [Feat(김유진(유자)): 미션 - 인증 실패 응답 통일 로직 util로 분리]

--- sophia22001/mission4/src/main/java/com/example/mission4/global/security/exception/CustomAccessDenied.java
+++ sophia22001/mission4/src/main/java/com/example/mission4/global/security/exception/CustomAccessDenied.java
@@ -1,14 +1,11 @@
 package com.example.mission4.global.security.exception;
 
-import com.example.mission4.global.apiPayload.ApiResponse;
-import com.example.mission4.global.apiPayload.code.BaseErrorCode;
 import com.example.mission4.global.apiPayload.code.GeneralErrorCode;
-import jakarta.servlet.ServletException;
+import com.example.mission4.global.security.util.SecurityResponseUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
-import tools.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 
@@ -21,17 +18,6 @@ public class CustomAccessDenied implements AccessDeniedHandler {
                        HttpServletResponse response,
                        AccessDeniedException accessDeniedException) throws IOException {
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        BaseErrorCode code = GeneralErrorCode.FORBIDDEN;
-
-        // 응답 Content-Type, HTTP 상태코드 정의
-        response.setContentType("application/json;charset=UTF-8");
-        response.setStatus(code.getStatus().value());
-
-        // Response Body에 응답 통일한 객체를 넣기
-        ApiResponse<Void> errorResponse = ApiResponse.onFailure(code, null);
-
-        // 실제 Response로 덮어쓰기
-        objectMapper.writeValue(response.getOutputStream(), errorResponse);
+        SecurityResponseUtil.writeErrorResponse(response, GeneralErrorCode.FORBIDDEN);
     }
 }
--- sophia22001/mission4/src/main/java/com/example/mission4/global/security/exception/CustomEntryPoint.java
+++ sophia22001/mission4/src/main/java/com/example/mission4/global/security/exception/CustomEntryPoint.java
@@ -1,14 +1,11 @@
 package com.example.mission4.global.security.exception;
 
-import com.example.mission4.global.apiPayload.ApiResponse;
-import com.example.mission4.global.apiPayload.code.BaseErrorCode;
 import com.example.mission4.global.apiPayload.code.GeneralErrorCode;
-import jakarta.servlet.ServletException;
+import com.example.mission4.global.security.util.SecurityResponseUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
-import tools.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 
@@ -22,18 +19,6 @@ public class CustomEntryPoint implements AuthenticationEntryPoint {
                          HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        BaseErrorCode code = GeneralErrorCode.UNAUTHORIZED;
-
-        // 응답 Content-Type, HTTP 상태코드 정의
-        response.setContentType("application/json;charset=UTF-8");
-        response.setStatus(code.getStatus().value());
-
-        // Response Body에 응답 통일한 객체를 넣기
-        ApiResponse<Void> errorResponse = ApiResponse.onFailure(code, null);
-
-        // 실제 Response로 덮어쓰기
-        objectMapper.writeValue(response.getOutputStream(), errorResponse);
-
+        SecurityResponseUtil.writeErrorResponse(response, GeneralErrorCode.UNAUTHORIZED);
     }
 }
