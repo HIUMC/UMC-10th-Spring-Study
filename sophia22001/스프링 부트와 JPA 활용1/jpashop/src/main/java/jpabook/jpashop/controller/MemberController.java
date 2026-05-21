5697d6579d2bbf51f93eb1056e5a31f09c084b19
Yujin Kim <yujii22001@gmail.com>
2026-05-21 14:30:41 +0900

    [Feat(김유진(유자)): 섹션 8. 회원 등록, 목록 조회]

---
+++ sophia22001/스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,54 @@
+package jpabook.jpashop.controller;
+
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    // 회원가입 폼
+    @GetMapping(value = "/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    // 회원가입
+    @PostMapping(value = "/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        // BindingResult: Validation 오류 잡기
+        if (result.hasErrors()) {
+            return "members/createMemberForm"; // 화면 다시 보이게
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/"; // 가입되면 홈으로 리다이렉트
+    }
+
+    // 회원 목록 조회
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+}
