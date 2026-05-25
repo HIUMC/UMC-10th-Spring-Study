cd5bc8fc428db4f17beb360ee166b5e2072e7861
9dongs <144160177+9dongs@users.noreply.github.com>
2026-05-26 02:13:17 +0900

    [Chore(구디) : JWT 패키지 설정 및 SecurityConfig 수정]

--- 9dongs/umc10th/src/main/java/com/example/umc10th/global/config/SecurityConfig.java
+++ 9dongs/umc10th/src/main/java/com/example/umc10th/global/config/SecurityConfig.java
@@ -2,6 +2,10 @@ package com.example.umc10th.global.config;
 
 import com.example.umc10th.global.auth.CustomAccessDenied;
 import com.example.umc10th.global.auth.CustomEntryPoint;
+import com.example.umc10th.global.security.filter.JwtAuthFilter;
+import com.example.umc10th.global.security.service.CustomUserDetailsService;
+import com.example.umc10th.global.security.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,10 +14,15 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService customUserDetailsService;
 
     private final String[] allowUris = {
             // Swagger 허용
@@ -27,27 +36,37 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                // URI 사용 여부
                 .authorizeHttpRequests(requests -> requests
+                        // Public API 허용
                         .requestMatchers(allowUris).permitAll()
+                        // 그 이외 API는 인증 필요
                         .anyRequest().authenticated()
                 )
-                .formLogin(form -> form
-                        .defaultSuccessUrl("/swagger-ui/index.html", true)
-                        .permitAll()
-                )
+
+                // 폼 로그인
+                .formLogin(AbstractHttpConfigurer::disable)
+
+                // JWT 필터
+                .addFilterBefore(jwtAuthFilter(), UsernamePasswordAuthenticationFilter.class)
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/login?logout")
                         .permitAll()
                 )
                 
-                // REST API 에러 응답 통일
+                // 예외 상황 핸들러
                 .exceptionHandling(exception -> exception
                          .accessDeniedHandler(customAccessDenied())
                          .authenticationEntryPoint(customEntryPoint())
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public JwtAuthFilter jwtAuthFilter() {
+        return new JwtAuthFilter(jwtUtil, customUserDetailsService);
     }
 
     @Bean
